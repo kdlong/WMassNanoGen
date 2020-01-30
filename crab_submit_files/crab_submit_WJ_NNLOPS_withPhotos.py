47da7fa1f763309822876e32322c73abaef4cb4d
Kenneth Long <kdlong@hep.wisc.edu>
2020-01-30 15:23:34 +0100

    Add NNLO-like file

--- crab_submit_files/crab_submit_WJ_NNLOPS_withPhotos.py
+++ crab_submit_files/crab_submit_WJ_NNLOPS_withPhotos.py
@@ -1,13 +1,13 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'WplusJToMuNu_TuneCUETP8M1_13TeV-powheg-NNLOPS-pythia8-photos'
+config.General.requestName = 'WplusJToMuNu_TuneCUETP8M1_fixWeights_13TeV-powheg-NNLOPS-pythia8-photos'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.psetName = '../WJ_NNLOPS_withPhotos_cfg.py'
+config.JobType.psetName = '../configs/WJ_NNLOPS_withPhotos_cfg.py'
 
 config.Data.outputPrimaryDataset = 'WplusJToMuNu_TuneCUETP8M1_13TeV-powheg-NNLOPS-pythia8-photos'
 config.Data.splitting = 'EventBased'
