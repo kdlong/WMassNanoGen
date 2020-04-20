c848c2cee64a1b5479a1af65bf7c30e25b980ed7
Kenneth Long <kdlong@hep.wisc.edu>
2020-04-20 12:07:34 +0200

    Fix plhe config

--- configs/ZToTauTau_madgraph_plheOnly_cfg.py
+++ configs/ZToTauTau_madgraph_plheOnly_cfg.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: Configuration/WMassNanoGen/python/ZToTauTau_madgraph_cff.py --fileout file:test2.root --mc --eventcontent NANOAODSIM --filein file:/data/kelong/MadGraph-NotCMS/TestDYTausMG27/Events/run_06/unweighted_events.lhe --datatier NANOAOD --conditions auto:mc --step LHE --python_filename configs/ZToTauTau_madgraph_cfg.py -n 10 --no_exec
+# with command line options: Configuration/test/python/ZToTauTau_madgraph_cff.py --fileout file:test2.root --mc --eventcontent NANOAODSIM --filein file:/data/kelong/MadGraph-NotCMS/TestDYTausMG27/Events/run_06/unweighted_events.lhe --datatier LHE --conditions auto:mc --step LHE --python_filename ZToTauTau_madgraph_plheOnly_cfg.py -n -1 --no_exec
 import FWCore.ParameterSet.Config as cms
 
 
@@ -21,8 +21,7 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10),
-    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+    input = cms.untracked.int32(-1)
 )
 
 # Input source
@@ -31,35 +30,12 @@ process.source = cms.Source("LHESource",
 )
 
 process.options = cms.untracked.PSet(
-    FailPath = cms.untracked.vstring(),
-    IgnoreCompletely = cms.untracked.vstring(),
-    Rethrow = cms.untracked.vstring(),
-    SkipEvent = cms.untracked.vstring(),
-    allowUnscheduled = cms.obsolete.untracked.bool,
-    canDeleteEarly = cms.untracked.vstring(),
-    emptyRunLumiMode = cms.obsolete.untracked.string,
-    eventSetup = cms.untracked.PSet(
-        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
 
-        ),
-        numberOfConcurrentIOVs = cms.untracked.uint32(1)
-    ),
-    fileMode = cms.untracked.string('FULLMERGE'),
-    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
-    makeTriggerResults = cms.obsolete.untracked.bool,
-    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
-    numberOfConcurrentRuns = cms.untracked.uint32(1),
-    numberOfStreams = cms.untracked.uint32(0),
-    numberOfThreads = cms.untracked.uint32(1),
-    printDependencies = cms.untracked.bool(False),
-    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
-    throwIfIllegalParameter = cms.untracked.bool(True),
-    wantSummary = cms.untracked.bool(False)
 )
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    annotation = cms.untracked.string('Configuration/WMassNanoGen/python/ZToTauTau_madgraph_cff.py nevts:10'),
+    annotation = cms.untracked.string('Configuration/test/python/ZToTauTau_madgraph_cff.py nevts:-1'),
     name = cms.untracked.string('Applications'),
     version = cms.untracked.string('$Revision: 1.19 $')
 )
@@ -70,7 +46,7 @@ process.NANOAODSIMoutput = cms.OutputModule("NanoAODOutputModule",
     compressionAlgorithm = cms.untracked.string('LZMA'),
     compressionLevel = cms.untracked.int32(9),
     dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string('NANOAOD'),
+        dataTier = cms.untracked.string('LHE'),
         filterName = cms.untracked.string('')
     ),
     fileName = cms.untracked.string('file:test2.root'),
@@ -83,22 +59,12 @@ process.NANOAODSIMoutput = cms.OutputModule("NanoAODOutputModule",
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')
 
-process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('dummy'),
-    nEvents = cms.untracked.uint32(10),
-    numberOfParameters = cms.uint32(1),
-    outputFile = cms.string('cmsgrid_final.lhe'),
-    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
-)
-
-
 # Path and EndPath definitions
-process.lhe_step = cms.Path(process.externalLHEProducer)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.NANOAODSIMoutput_step = cms.EndPath(process.NANOAODSIMoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.lhe_step,process.endjob_step,process.NANOAODSIMoutput_step)
+process.schedule = cms.Schedule(process.endjob_step,process.NANOAODSIMoutput_step)
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
 
