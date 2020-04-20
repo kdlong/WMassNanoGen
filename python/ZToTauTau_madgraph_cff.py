d586ce5a9a212a84fccd503b5fd8afde8bbb1ea3
Kenneth Long <kdlong@hep.wisc.edu>
2020-04-20 20:45:13 +0200

    More configs, cmsDriver from plhe

---
+++ python/ZToTauTau_madgraph_cff.py
@@ -0,0 +1,57 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        pythia8PowhegEmissionVetoSettingsBlock,
+        processParameters = cms.vstring(
+            'POWHEG:nFinal = 2',   ## Number of final state particles
+                                    ## (BEFORE THE DECAYS) in the LHE
+                                    ## other than emitted extra parton
+            'ParticleDecays:allowPhotonRadiation = on',
+            'TimeShower:QEDshowerByL = off',
+            ),
+		parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8PowhegEmissionVetoSettings',
+                                    'pythia8PSweightsSettings',
+                                    'processParameters')
+    ),
+	ExternalDecays = cms.PSet(
+        Photospp = cms.untracked.PSet(
+            parameterSets = cms.vstring("setExponentiation", "setInfraredCutOff", "setMeCorrectionWtForW", "setMeCorrectionWtForZ", "setMomentumConservationThreshold", "setPairEmission", "setPhotonEmission", "setStopAtCriticalError", "suppressAll", "forceBremForDecay"),
+            setExponentiation = cms.bool(True),
+            setMeCorrectionWtForW = cms.bool(True),
+            setMeCorrectionWtForZ = cms.bool(True),
+            setInfraredCutOff = cms.double(0.00011),
+            setMomentumConservationThreshold = cms.double(0.1),
+            setPairEmission = cms.bool(True),
+            setPhotonEmission = cms.bool(True),
+            setStopAtCriticalError = cms.bool(False),
+            # Use Photos only for W/Z decays
+            suppressAll = cms.bool(True),
+            forceBremForDecay = cms.PSet(
+                parameterSets = cms.vstring("Z", "Wp", "Wm"),
+                Z = cms.vint32(0, 23),
+                Wp = cms.vint32(0, 24),
+                Wm = cms.vint32(0, -24),
+            ),
+        ),
+        parameterSets = cms.vstring("Photospp")
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
+
