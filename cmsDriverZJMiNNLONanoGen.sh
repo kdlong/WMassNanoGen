7b47d849203e805b3168b6785f1ba7d373a57950
Kenneth Long <kdlong@hep.wisc.edu>
2020-05-20 15:02:50 +0200

    Add generic cmsDriver scipt

--- cmsDriverZJMiNNLONanoGen.sh
+++ cmsDriverZJMiNNLONanoGen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cmsDriver.py step1 --filein "dbs:/ZJToMuMu_TuneCUETP8M1_13TeV-powheg-MiNNLO-pythia8-photos/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v1/MINIAODSIM" \
-    --fileout ZJToMuMu_TuneCUETP8M1_13TeV-powheg-MiNNLO-pythia8-photos_NanoGen.root --mc --eventcontent NANOAODGEN --datatier NANOAODSIM \
-    --conditions 102X_mcRun2_asymptotic_v7 --step NANOGEN --nThreads 4 --python_filename configs/ZJToMuMu_TuneCUETP8M1_13TeV-powheg-MiNNLO-pythia8-photos_NanoGen.py \
+cmsDriver.py step1 --filein "dbs:/ZJToMuMu_mWPilot_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v1/MINIAODSIM" \
+    --fileout ZJToMuMu_mWPilot_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos_NanoGen.root --mc --eventcontent NANOAODGEN --datatier NANOAODSIM \
+    --conditions 102X_mcRun2_asymptotic_v7 --step NANOGEN --nThreads 4 --python_filename configs/ZJToMuMu_mWPilot_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos_NanoGen.py \
     --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 10000 
