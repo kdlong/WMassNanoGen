ee625d4fbcda38acf588385709ef783a83194e5c
Kenneth Long <kdlong@hep.wisc.edu>
2020-09-25 11:13:37 +0200

    Add crab files for nano

---
+++ crab_submit_files/crab_submit_ZJ_MiNNLO_svn3756_MarkusFix_Q01p39_Wimpy_primKt2p2_FullSimGeantDefaul_NANO.py
@@ -0,0 +1,24 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'ZJToMuMu_svn3756_ProductionCand_GeantDefault_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos_ext1_NANO'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'ZJ_MiNNLO_svn3756_MarkusFix_Q01p39_PrimKt2p2_noWeights_Photos_fullSim_NANOAOD_cfg.py'
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.numCores = 8
+config.JobType.maxMemoryMB = 16000
+config.JobType.maxJobRuntimeMin = 180
+
+config.Data.inputDataset = '/ZJToMuMu_svn3756_ProductionCand_GeantDefault_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos_ext1/kelong-RunIISummer20ULMINIAODSIM-35016d68ab4dd304e67d9c7d41b75a31/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 50
+config.Data.outLFNDirBase = '/store/group/cmst3/user/kelong/' 
+config.Data.publication = True
+config.Data.outputDatasetTag = 'RunIISummer20ULNANOAODSIM'
+
+config.Site.storageSite = 'T2_CH_CERN'
