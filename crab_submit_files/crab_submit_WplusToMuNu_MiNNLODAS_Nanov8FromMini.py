64d23b32cd206eec7af4ac03e9d95c7eba008c64
Kenneth Long <kdlong@hep.wisc.edu>
2020-11-03 00:00:34 +0100

    Add config file with Nano updates

---
+++ crab_submit_files/crab_submit_WplusToMuNu_MiNNLODAS_Nanov8FromMini.py
@@ -0,0 +1,24 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'WplusJetsToMuNu_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.numCores = 4
+config.JobType.maxMemoryMB = 8000
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '../configs/SMP-RunIISummer16NanoAODv8-wmassNano_cfg.py'
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/WplusJetsToMuNu_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos/RunIISummer20UL16MiniAODAPV-106X_mcRun2_asymptotic_preVFP_v8-v2/MINIAODSIM'
+config.Data.allowNonValidInputDataset = True
+
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 4
+config.Data.outLFNDirBase = '/store/group/cmst3/group/wmass/w-mass-13TeV/NanoAOD' 
+config.Data.publication = True
+config.Data.outputDatasetTag = 'NanoAODv8'
+
+config.Site.storageSite = 'T2_CH_CERN'
