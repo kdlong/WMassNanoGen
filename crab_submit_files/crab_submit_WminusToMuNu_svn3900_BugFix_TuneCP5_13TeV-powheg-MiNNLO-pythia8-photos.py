74b83187485b3e3e4d0b32a406eb605ef8eeec4d
Kenneth Long <kdlong@hep.wisc.edu>
2022-03-07 23:59:50 +0000

    Add bug fix MiNNLO samples

--- crab_submit_files/crab_submit_WminusToMuNu_svn3900_BugFix_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos.py
+++ crab_submit_files/crab_submit_WminusToMuNu_svn3900_BugFix_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos.py
@@ -1,19 +1,21 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'DYJetsToMuMu_svn3900_BugFix_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos'
+config.General.requestName = 'WminusToMuNu_svn3900_BugFix_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
+config.JobType.numCores = 4
+config.JobType.maxMemoryMB = 2000*4
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.psetName = '../configs/DYJetsToMuMu_svn3900_BugFix_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos_cfg.py'
+config.JobType.psetName = '../configs/WminusJetsToMuNu_svn3900_BugFix_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos_cfg.py'
 config.JobType.allowUndistributedCMSSW = True
 
-config.Data.outputPrimaryDataset = 'DYJetsToMuMu_svn3900_BugFix_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos'
+config.Data.outputPrimaryDataset = 'WminusToMuNu_svn3900_BugFix_TuneCP5_13TeV-powheg-MiNNLO-pythia8-photos'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 600
-NJOBS = 5000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.unitsPerJob = 3500
+NJOBS = 4000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/group/cmst3/group/wmass/w-mass-13TeV/NanoGen/' 
 config.Data.publication = True
